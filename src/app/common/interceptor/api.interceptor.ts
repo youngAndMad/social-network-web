9a7a4a17590880ff8e169d0e738d31f5d618ecf1
youngandmad <kkraken2005@gmail.com>
2024-02-13 06:19:38 +0600

    dev keycloak

--- src/app/common/interceptor/api.interceptor.ts
+++ src/app/common/interceptor/api.interceptor.ts
@@ -3,7 +3,7 @@ import {
   HttpRequest,
   HttpHandler,
   HttpEvent,
-  HttpInterceptor
+  HttpInterceptor,
 } from '@angular/common/http';
 import { Observable } from 'rxjs';
 import { EnvService } from '../service/env.service';
@@ -11,24 +11,26 @@ import { KeycloakService } from 'keycloak-angular';
 
 @Injectable()
 export class ApiInterceptor implements HttpInterceptor {
-
   constructor(
     private readonly _envService: EnvService,
     private readonly _keycloak: KeycloakService
-  ) { }
+  ) {}
 
-  intercept(request: HttpRequest<unknown>, next: HttpHandler): Observable<HttpEvent<unknown>> {
+  intercept(
+    request: HttpRequest<unknown>,
+    next: HttpHandler
+  ): Observable<HttpEvent<unknown>> {
     const token = this._keycloak.getKeycloakInstance().token;
 
     if (request.url.includes('api')) {
       const modifiedReq = request.clone({
         url: this._envService.apiUrl + request.url,
         setHeaders: {
-          Authorization: `Bearer ${token}`
-        }
-      })
+          Authorization: `Bearer ${token}`,
+        },
+      });
       return next.handle(modifiedReq);
     }
-    return next.handle(request)
+    return next.handle(request);
   }
 }
--- src/environments/environment.ts
+++ src/environments/environment.ts
@@ -1,10 +1,10 @@
-import { IEnvironment } from "./environment.interface";
+import { IEnvironment } from './environment.interface';
 
 export const environment: IEnvironment = {
-    apiUrl: 'http://localhost:9393',
-    websocketUrl: 'http://localhost:9393/api/v1/chat-service/social-app',
-    kcClientId: 'socialapp-web',
-    kcRealm: 'socialapp',
-    kcRedirectUrl: 'http://localhost:4200',
-    kcIssuerUrl:'http://localhost:8080'
-}
+  apiUrl: 'http://localhost:7000',
+  websocketUrl: 'http://localhost:9393/api/v1/chat-service/social-app',
+  kcClientId: 'socialapp-web',
+  kcRealm: 'socialapp',
+  kcRedirectUrl: 'http://localhost:4200',
+  kcIssuerUrl: 'http://localhost:8080',
+};
