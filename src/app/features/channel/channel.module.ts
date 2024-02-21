c65545c02a88845c7798664054fee15e2840d804
youngandmad <kkraken2005@gmail.com>
2024-02-22 02:08:57 +0600

    channel module init as they say

---
+++ src/app/features/channel/channel.module.ts
@@ -0,0 +1,14 @@
+import { NgModule } from '@angular/core';
+import { CommonModule } from '@angular/common';
+
+import { ChannelRoutingModule } from './channel-routing.module';
+
+
+@NgModule({
+  declarations: [],
+  imports: [
+    CommonModule,
+    ChannelRoutingModule
+  ]
+})
+export class ChannelModule { }
